1048560cf9f42f511571400383174b11870cc6d2
devarshi-patidar1812 <devarshi330@gmail.com>
2024-03-08 22:27:25 +0530

    Constructor Injection

---
+++ codes/SpringFramework/src/main/java/com/spring/demo/SpringFramework/constructorinjection/Person.java
@@ -0,0 +1,30 @@
+package com.spring.demo.SpringFramework.constructorinjection;
+
+import java.util.List;
+
+public class Person {
+	
+	private String name;
+	private int personid;
+	private Certi certi;
+//	private List<String> list;
+
+	public Person(String name,int personid,Certi certi) {
+		this.name = name;
+		this.personid = personid;
+		this.certi = certi;
+//		this.list = list;
+	}
+
+	@Override
+	public String toString() {
+		
+		return 
+				this.name
+				+" : "
+				+ this.personid
+				+ "{" + this.certi.name + "}";
+	}
+	
+
+}
